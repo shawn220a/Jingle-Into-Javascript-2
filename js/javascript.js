eb6b776caf024da32ed2f93b43775ec1bcbdc9f9
shawn220a <shawn.tschoepe@icloud.com>
2019-10-20 18:28:23 -0500

    Got every button except the fade button working properly.

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+// Button Grows
+document.getElementById("button1").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "250px";});
+
+// Button Turns Blue
+document.getElementById("button2").addEventListener("click", function(){
+
+document.getElementById("box").style.backgroundColor = "Blue";});
+
+// Button Fades
+document.getElementById("button3").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "150px";});
+
+
+// Button Resets
+document.getElementById("button4").addEventListener("click", function(){
+
+document.getElementById("box").style.height = "150px"
+document.getElementById("box").style.backgroundColor = "Orange";});
