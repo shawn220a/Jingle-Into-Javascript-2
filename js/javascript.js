2ec998c73daa4e4c1f74289b34cc9d98143c52ee
shawn220a <shawn.tschoepe@icloud.com>
2019-10-20 21:22:25 -0500

    Made the image fade when you click the fade button.

--- js/javascript.js
+++ js/javascript.js
@@ -10,12 +10,13 @@ document.getElementById("box").style.backgroundColor = "Blue";});
 
 // Button Fades
 document.getElementById("button3").addEventListener("click", function(){
-
-document.getElementById("box").style.height = "150px";});
+document.getElementById("box").style.opacity = .5;
+});
 
 
 // Button Resets
 document.getElementById("button4").addEventListener("click", function(){
 
 document.getElementById("box").style.height = "150px"
-document.getElementById("box").style.backgroundColor = "Orange";});
+document.getElementById("box").style.backgroundColor = "Orange";
+document.getElementById("box").style.opacity = 1;});
